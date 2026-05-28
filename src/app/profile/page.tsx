5b23443c250d6c80378d3c96adbc53478dbedd88
Mubeen Khan <mubix.dev@gmail.com>
2026-05-29 00:22:23 +0500

    Add profile page component

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full min-h-screen flex justify-center items-center'>
+        Profile page
+    </div>
+  )
+}
+
+export default page
