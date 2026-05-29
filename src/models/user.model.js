69a940cc77619694473ae7e8b51f758779130cf8
Mubeen Khan <mubix.dev@gmail.com>
2026-05-30 00:03:43 +0500

    Update user model to include custom error messages for required fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,17 +4,17 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true,"Please provide username"],
       unique: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true,"Please provide email"],
       unique: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true,"Please provide password"],
     },
     isAdmin: {
       type: Boolean,
