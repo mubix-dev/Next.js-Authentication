809f7daf51bc435ddeb0adfc767d6130c6cec3bf
Mubeen Khan <mubix.dev@gmail.com>
2026-05-30 00:02:57 +0500

    Add user login API route with JWT authentication

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,60 @@
+import connectDB from "@/config/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextResponse, NextRequest } from "next/server";
+import jwt from "jsonwebtoken"
+
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const reqBody = await req.json();
+
+    const {email, password } = reqBody;
+    if (!email || !password) {
+      return NextResponse.json(
+        { message: "Please provide complete details" },
+        { status: 400 },
+      );
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User not fond!" },
+        { status: 400 },
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(!isPasswordValid){
+        return NextResponse.json(
+        { error: "Invalid credentials!" },
+        { status: 400 },
+      );
+    }
+
+    const securedUser = await User.findOne({email : user.email}).select("-password");
+
+    const tokenData = {
+        id:securedUser._id,
+        username:securedUser.username,
+        email:securedUser.email
+    }
+
+    const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET!,{expiresIn:"1d"})
+    const response =  NextResponse.json({
+        message:"User login successfully!",
+        success:true,
+        createdUser: securedUser
+    },{status:200})
+    
+    response.cookies.set("token",token,{
+        httpOnly:true
+    })
+
+    return response;
+
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
