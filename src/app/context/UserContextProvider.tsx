29f3b96a2deeb0db8fd47a532fc3548d3a298760
Mubeen Khan <mubix.dev@gmail.com>
2026-05-31 23:42:55 +0500

    Add UserContextProvider for user data management and retrieval

---
+++ src/app/context/UserContextProvider.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { createContext,useEffect,useState } from "react";
+import axios from "axios";
+
+export const dataContext = createContext(null as any)
+
+function UserContextProvider({children}:any) {
+  const [userData,setUserData] = useState(null);
+  useEffect(() => {
+    if(userData){
+      return;
+    }
+    const fetchCurrUser = async () => {
+      try {
+        const result: any = await axios.get("/api/users/curr-user");
+        console.log(result.data.data);
+        setUserData(result.data.data);
+      } catch (error: any) {
+        console.log(error.response?.data?.message);
+      }
+    };
+
+    fetchCurrUser();
+  }, [userData,setUserData]);
+  return (
+    <div>
+      <dataContext.Provider value={{ userData, setUserData }}>
+      {children}
+      </dataContext.Provider>
+    </div>
+  )
+}
+
+export default UserContextProvider;
