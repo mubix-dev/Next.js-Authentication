fdd75f8792df90d1ef965902bbc9b9baeca9b975
Mubeen Khan <mubix.dev@gmail.com>
2026-06-05 01:57:31 +0500

    Remove unnecessary blank line in middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,6 @@ export function middleware(request: NextRequest) {
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
   }
-
   if (!isPublicPath && !isRootPath && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
